d8e5395c8398d20ddfdeca4cc33983096c8d8085
Drew Tada <drewtada@drews-mbp.myfiosgateway.com>
2021-05-09 02:37:29 -0400

    added great works of art

---
+++ pages/writing/great-works.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+export default function GreatWorks() {
+  return (
+    <Layout>
+      <h1>Great Works of Art</h1>
+      <h3>
+        This is a continuously updated list of great works of art. 1 per artist. Excluded artists may or
+        may not be intentional.
+      </h3>
+      <h1>Paintings</h1>
+      <ul class="block-list">
+        <li>Sistine Chapel - Michelangelo</li>
+        <li>Potato Eaters - Van Gogh</li>
+        <li>The Garden of Earthly Delights - Bosch</li>
+        <li>Death of Marat - David</li>
+        <li>Night Watch - Rembrandt</li>
+        <li>The Course of Empire - Thomas Cole</li>
+        <li>The Fighting Temeraire - Turner</li>
+        <li>Las Meninas - Velazquez</li>
+        <li>The Resurrection - El Greco</li>
+        <li>School of Athens - Raphael</li>
+        <li>The Calling of Saint Matthew - Caravaggio</li>
+        <li>Lilypads - Monet</li>
+        <li>Liberty Leading the People - Delacroix</li>
+        <li>Vampire - Munch</li>
+        <li>Wanderer Above the Sea of Fog - David Casper Friedrich</li>
+        <li>The Kiss - Kilmpt</li>
+        <li>The Last Supper - Da Vinci</li>
+        <li>The Great Wave - Hokusai</li>
+      </ul> 
+      <h1>Sculpture</h1>
+      <ul class="block-list">
+        <li>The Ecstacy of St. Teresa - Bernini</li>
+        <li>David - Michelangelo</li>
+        <li>David - Donatello</li>
+        <li>Pieta - Michelangelo</li>
+        <li>The Gates of Hell - Rodin</li>
+        <li>Laocoön and His Sons - Unknown</li>
+        <li>Venus de Milo - Unknown</li>
+      </ul>
+      <h1>Architecture</h1>
+      <p><i>Working on it...</i></p>
+      <h3 class="back-button">
+        <Link href="/writing">Back</Link>
+      </h3>
+    </Layout>
+  )
+}
--- pages/writing/index.js
+++ pages/writing/index.js
@@ -10,8 +10,8 @@ export default function Home() {
       <h2>Culture: Atoms v Bits</h2>
       <p>
         Our cultural production is entirely virtual. Every cultural artifact we produce (film, television, videogames, 
-        music), are enjoyed through a screen. There are no great architects, painters, or sculptors, i.e. people that
-        shape the real world. Those that refuse to flatten their work into a bytestream have no cultural capital.
+        music), is enjoyed through a screen. There are no great architects, painters, or sculptors, left.
+        Those that refuse to flatten their work into a bytestream have no cultural capital.
         And so it seems everything beautiful is not real, and everything real is not beautiful.
       </p>
       <h2>Internet Archaeology</h2>
@@ -24,13 +24,15 @@ export default function Home() {
       <p>
         Crpyto and NFTs are a frontier where artist/patron relationships can florish. While the critics are correct that
         the current environment wreaks of fraud, shallow work, and tulip mania, we are still in the early days of
-        crypto-patronage. It is up to a small community of artists to raise the standard and redefine what capital-A Art isß.
+        crypto-patronage. It is up to a small community of artists to raise the standard and redefine what capital-A Art is.
       </p>
       <br/>
       <h1>Lists</h1>
       <h2>リスト</h2>
       <ul class="block-list">
         <li><Link href="/writing/reading-list">Required Reading</Link></li>
+        <li><Link href="/writing/great-works">Great Works of Art</Link></li>
+
       </ul>
     </Layout>
   );
